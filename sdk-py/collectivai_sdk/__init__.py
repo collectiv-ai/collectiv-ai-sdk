dc3d89103ca38ab457dad1dafe783bf83829ce48
CollectiVAI <info@collectivai.org>
2025-11-30 18:33:28 +0100

    Create __init__.py

---
+++ sdk-py/collectivai_sdk/__init__.py
@@ -0,0 +1,8 @@
+from .client import CollectivAIClient, ChatRequest, ChatResponse, RoutingInfo
+
+__all__ = [
+    "CollectivAIClient",
+    "ChatRequest",
+    "ChatResponse",
+    "RoutingInfo",
+]
